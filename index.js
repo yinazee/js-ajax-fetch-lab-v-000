88f40ff8d882f2019e02e6439480238180022ec2
github username <github email address>
2019-01-25 17:50:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-const token = 'token';
+const token = ' ';
+const baseURL = 'https://api.github.com';
+const = 'yinazee'
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return token;
 }
 
 
 function forkRepo() {
-  const token = getToken();
   //use fetch to fork it!
-  fetch(
-    'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks',
-    {
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+
       method: 'POST',
       headers: {
-        Authorization: `token ${token}`
+        Authorization: `token ${getToken()}`
       }
     }
   ).then(res => res.json())
-  .then(json => showResults(json));
+  .then(data => showResults(data));
 };
 
 
-function showResults(json) {
+function showResults(data) {
+  // debugger
   //use this function to display the results from forking via the API
-  document.getElementbyId('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
+  document.getElementById('results').innerHTML = `<a href=${data.html_url}>${data.html_url}</a>`;
 
 }
 
-function createIssue() {
-  //use this function to create an issue based on the values input in index.html
-}
+// function createIssue() {
+//   const repo = `${user}/js-ajax-fetch-lab`;
+//   const url = `${baseURL}/repos/${repo}/issues`;
+//   const postData = {
+//     title: document.getElementById('title').value,
+//     body: document.getElementById('body').value
+//   };
+//
+//   fetch(url, {
+//     method: 'POST',
+//     body: JSON.stringify(postData),
+//     headers: {
+//       Authorization: `token ${getToken()}`
+//     }
+//   })
+//     .then(res => res.json())
+//     .then(json => getIssues());
+// }
+
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
