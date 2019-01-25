16982c42a133f29ec6a1bce6b79f6e5294465841
github username <github email address>
2019-01-25 15:56:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+const token = 'token';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return '';
 }
 
+
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const token = getToken();
   //use fetch to fork it!
-}
+  fetch(
+    'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${token}`
+      }
+    }
+  ).then(res => res.json())
+  .then(json => showResults(json));
+};
+
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  document.getElementbyId('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
+
 }
 
 function createIssue() {
